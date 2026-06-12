54c1d8c7fdcf4abadb9fa25ff1b21803bd8bedba
Penygonarabia <arun@penygonarabia.com>
2026-06-12 11:51:49 +0530

    quoataion is added

--- hhs_amc_quotation/models/crm_sales_quotation_scope_work.py
+++ hhs_amc_quotation/models/crm_sales_quotation_scope_work.py
@@ -41,12 +41,12 @@ class ServiceSaleOrder(models.Model):
         default=lambda self: self._default_late_payment_note()
     )
     
-    terms_of_execution = fields.Text(string="Terms of Execution", default = lambda self:self.env['ir.config_parameter'].sudo().get_param('crm_custom_view.terms_of_execution').replace('\\n', '\n'),deprecated = False)
-    exclusions_text = fields.Text(string="Exclusions", default = lambda self:self.env['ir.config_parameter'].sudo().get_param('crm_custom_view.exclusions').replace('\\n', '\n',deprecated = False))
+    terms_of_execution = fields.Text(string="Terms of Execution", default = lambda self:self.env['ir.config_parameter'].sudo().get_param('crm_custom_view.terms_of_execution').replace('\\n', '\n'))
+    exclusions_text = fields.Text(string="Exclusions", default = lambda self:self.env['ir.config_parameter'].sudo().get_param('crm_custom_view.exclusions').replace('\\n', '\n'))
     enable_scope = fields.Boolean(string="Enable Scope")
 
     '''Code Added on March 21 2026 by Vijaya bhaskar'''
-    others_text = fields.Text(string="Notes", default = lambda self:self.env['ir.config_parameter'].sudo().get_param('crm_custom_view.notes').replace('\\n', '\n'),deprecated = False)
+    others_text = fields.Text(string="Notes", default = lambda self:self.env['ir.config_parameter'].sudo().get_param('crm_custom_view.notes').replace('\\n', '\n'))
 
 
     @api.model
@@ -71,11 +71,11 @@ class ServiceSaleOrder(models.Model):
     )
 
     # 20260408 Gokul
-    subject = fields.Char(string="Subject", default=lambda self: self.env['ir.config_parameter'].sudo().get_param(
-        'crm_custom_view.subject').replace('\\n', '\n'),deprecated = False)
-    scope_of_work = fields.Char(string="Scope of Work", default = lambda self : self.env['ir.config_parameter'].sudo().get_param('crm_custom_view.scope_of_work').replace('\\n', '\n'),deprecated = False)
+    subject = fields.Text(string="Subject", default=lambda self: self.env['ir.config_parameter'].sudo().get_param(
+        'crm_custom_view.subject').replace('\\n', '\n'))
+    scope_of_work = fields.Text(string="Scope of Work", default = lambda self : self.env['ir.config_parameter'].sudo().get_param('crm_custom_view.scope_of_work').replace('\\n', '\n'))
     
-    others = fields.Char(string="Others",default = lambda self : self.env['ir.config_parameter'].sudo().get_param('crm_custom_view.others_txt').replace('\\n', '\n'),deprecated = False)
+    others = fields.Text(string="Others",default = lambda self : self.env['ir.config_parameter'].sudo().get_param('crm_custom_view.others_txt').replace('\\n', '\n'))
 
 
     @api.depends('grand_total_amount')
